bce45c844e0fe2706e262dd3b4190615808038a0
timedwansson <timedwansson@gmail.com>
2021-03-31 18:23:02 +0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User, validate, validateCards } = require("../models/user");
+const { Card } = require('../models/card');
+const auth = require("../middleware/auth");
+const router = express.Router();
+
+const getCards = async(cardsArray) => {
+    return await Card.find({ bizNumber: { $in: cardsArray } });
+};
+
+router.patch("/cards", auth, async(req, res) => {
+    // Validate body
+    const { error } = validateCards(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    //validate bizNumber 
+    const cards = await getCards(req.body.cards);
+    if (cards.length != req.body.cards.length) {
+        return res.status(400).send("Card number does not match");
+    }
+
+    //update user cards
+    let user = await User.findById(req.user._id);
+    user.cards = user.cards ? [...req.body.cards, ...user.cards] : req.body.cards;
+    user = await user.save();
+    res.send(user);
+
+});
+
+router.get("/me", auth, async(req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+    res.send(user);
+});
+
+router.post("/", async(req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send("Email is taken!!!");
+
+    user = new User(req.body);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+    res.send(_.pick(user, ["_id", "name", "email"]));
+});
+
+module.exports = router;
